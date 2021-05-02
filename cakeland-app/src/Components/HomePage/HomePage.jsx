5f31fb048913c1327582e507d1dc68e65b699c41
sanket <sanketsabale9767@gmail.com>
2021-05-02 14:36:07 +0530

    working on crwn-cloth Routing

---
+++ cakeland-app/src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HomePage = () => {
+    return ( 
+        <div className="homepage">
+            
+        </div>
+     );
+}
+ 
+export default HomePage;
---
+++ crwn-cloth/src/App.js
@@ -0,0 +1,30 @@
+import { Route, Switch } from 'react-router';
+import './App.css';
+import HomePage from './Pages/Homepage/homepage.component';
+
+function App() {
+
+  const HatsPage = () => (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+
+  const TopicPageDetails = (props) => (
+    <div>
+      <h1>Topic Page {props.match.params.topicId}</h1>
+    </div>
+  )
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route  path='/shop/hats' component={HatsPage}/>
+        <Route path="/topic/:topicId" component={TopicPageDetails}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
