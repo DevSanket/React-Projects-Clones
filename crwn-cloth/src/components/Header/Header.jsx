da5ae407ee0c2b90b70df384bebee2f33ed156bf
sanket <sanketsabale9767@gmail.com>
2021-05-07 08:45:48 +0530

    cake admin removed

--- crwn-cloth/src/components/Header/Header.jsx
+++ crwn-cloth/src/components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = ({currentUser,hidden}) => {
                 {
                     currentUser ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> : <Link to="signin">SIGN IN</Link>
                 }
-                <CartIcon />
+                <CartIcon className="cart-icon" />
             </div>
             {
                 hidden ? null : <CartDropDown/>
