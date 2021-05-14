b3ea984d20a458aeae9f5009b6abc5c2893a19cf
sanket <sanketsabale9767@gmail.com>
2021-05-14 15:00:05 +0530

    created cart item component and connected cart item reducer in crwn cloth

--- crwn-cloth/src/components/cart-dropdown/cart-dropdown.jsx
+++ crwn-cloth/src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item';
 import CustomButton from '../CustomButton/custom-button';
 import './cart-dropdown.scss';
 
 
-const CartDropDown = () => {
+const CartDropDown = ({cartItems}) => {
     return ( 
         <div className="cart-dropdown">
-            <div className="cart-items"></div>
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+                }
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
      );
 }
+
+const mapStateToProps = ({cart : {cartItems}}) => ({
+    cartItems
+});
  
-export default CartDropDown;
+export default connect(mapStateToProps)(CartDropDown);
